e59a6dc6455b74064b65c1c64ab71797e2cbd4b7
Eric Wu <ewu40@stuy.edu>
2024-01-05 14:36:40 -0500

    Straying away from semaphore, just focusing on making the network run, currently testing if client commands will go to server

--- server.c
+++ server.c
@@ -1,27 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/shm.h> 
+#include <unistd.h>
 #include "networking.h"
-
-void rot13(char * string){
-  for (int i = 0; i < strlen(string); ++i) {
-    if ((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z')) {
-      if (string[i] > 'm' || (string[i] > 'M' && string[i] <= 'Z')) {
-        string[i] -= 13;
-      } 
-      else {
-        string[i] += 13;
-      }
-    }
-  }
-}
+#define KEY 24602
+#define SHMKEY 24605
 
 void subserver_logic(int client_socket){
-  printf("Listening to the client\n");
+  printf("Listening to the client commands\n");
   char msgRead[BUFFER_SIZE];
   read(client_socket,msgRead,sizeof(msgRead));
+  if (strcmp(msgRead,"write")){
+    printf("1");
+  }
   //printf("Read: %s",msgRead);
-  char rot13Msg[BUFFER_SIZE];
-  rot13(msgRead);
-  strcpy(rot13Msg,msgRead);
-  write(client_socket,msgRead,sizeof(msgRead));
+  // char rot13Msg[BUFFER_SIZE];
+  // strcpy(rot13Msg,msgRead);
+  // write(client_socket,msgRead,sizeof(msgRead));
   //printf("Wrote: %s",rot13Msg);
 }
 
@@ -29,6 +30,13 @@ int main(int argc, char *argv[] ) {
   int listen_socket = server_setup(); 
   int client_socket = server_tcp_handshake(listen_socket);
   subserver_logic(client_socket);
+
+  // char data[128];
+  // int bytes; 
+  // int semd = semget(KEY, 1, 0); //Getting value of semaphore
+  // int shmid = shmget(SHMKEY, sizeof(int), IPC_CREAT | 0640); //Getting value of shared memory
+  // shmctl(shmid, IPC_RMID, 0); //Removing the shared memory
+  // semctl(semd, IPC_RMID, 0);
 
   // char command[BUFFER_SIZE];
   // printf("Enter a command: \n"); 
