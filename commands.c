440dae2147ab7869f45f874943886844d605a45d
RuizeLin <rlin41@stuy.edu>
2024-01-05 01:06:49 -0500

    mod. read and edit in commands.c

--- commands.c
+++ commands.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define MAX 2000
+#include <string.h>
+#define MAX 256
 
 //______________________________DATABASE_MANIPULATION______________________________
 // read from server and print out the content in the the database
-int read(int server_socket) {
+void read(int server_socket) {
     char buffer[MAX];
     read(server_socket, buffer, MAX);
     printf("%s\n", buffer);
-    return 0;
 }
 
 // update database in the server with user prompt
-int edit(int server_socket) {
+void edit(int server_socket) {
     char buffer[MAX];
-    // Operation prompt
-    printf("Enter the operation (add, update, delete) you would like to perform\n: ");
-    fgets(buffer, MAX, stdin);
-    
+    printf("For the list of possible edit commands, enter: help\n");
+    int n = -1;
+    while(n = -1) {
+        // user prompt
+        printf("Enter the edit you would like to make in the format [operation options entries]\n: ");
+        fgets(buffer, MAX, stdin);
 
+        if(strcmp(buffer, "help\n") == 0) { // help
+            printf("OPERATIONS\n");
+            printf("\tadd\n\tupdate\n\tdelete\n");
+            printf("OPTIONS\n");
+            printf("\t-col\n\t-row\n\t-cel\t*used in combination with update only\n");
+            printf("ENTRIES\n");
+            printf("\teach entry is separated by single space\n");
+        }
+        else {
+            n = 0;
+        }
+    }
     write(server_socket, buffer, MAX);
-    return 0;
 }
 
 //______________________________FILE_MANIPULATION______________________________
--- commands.h
+++ commands.h
@@ -1,10 +1,5 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
-struct command{
-    char command[16];
-    char operation[16];
-    char argument[256];
-};
 int read(int server_socket);
 int edit(int server_socket);
 #endif
