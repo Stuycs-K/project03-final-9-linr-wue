ebd4507f2efbf4d5ac55a02c57a21e0f01dc3b3d
ericw149 <eric667718@gmail.com>
2024-01-07 20:17:14 -0500

    Placed accessing semaphore code in clientLogic if the command is to edit; goal is to have one client edit at a time

--- client.c
+++ client.c
@@ -29,7 +29,33 @@ void clientLogic(int server_socket){
     cread_data(server_socket, msgToSend);
   }
   else if (strcmp(first_cmd, "edit") == 0) {
+    //Accessing semaphore
+    int semd;
+    int r;
+    int v;
+    int *data;
+    int shmid;
+    char buffer[128];
+    shmid = shmget(SHMKEY, 0, 0);//Gets shared memory
+    semd = semget(KEY, 1, 0);//Gets semaphore
+    if (semd == -1) { //Error in semaphore
+      printf("No semaphore exists!\n");
+      printf("error %d: %s\n", errno, strerror(errno));
+      semd = semget(KEY, 1, 0);
+      v = semctl(semd, 0, GETVAL, 0); 
+      printf("Semctl returned: %d\n", v); //Semaphore id will be 1
+    }
+    printf("Attempting to open semaphore.\n");
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_op = -1; //Using the semaphore by downing value of semaphore
+    semop(semd, &sb, 1); //Setting sempahore value to 1 so others can't use it
+    printf("Semaphore accessed!\n");
+
     cedit_data(server_socket, msgToSend);
+
+    sb.sem_op = 1; //Upping value of semaphore to indicate another program can use it
+    semop(semd, &sb, 1);
   }
   printf("\n");
   // write(server_socket, msgToSend, strlen(msgToSend)+1);
@@ -51,41 +77,41 @@ int main(int argc, char *argv[] ) {
     clientLogic(server_socket);
   }
 
-  int semd;
-  int r;
-  int v;
-  int *data;
-  int shmid;
-  char buffer[128];
+  // int semd;
+  // int r;
+  // int v;
+  // int *data;
+  // int shmid;
+  // char buffer[128];
 
-  shmid = shmget(SHMKEY, 0, 0);//Gets shared memory
-  semd = semget(KEY, 1, 0);//Gets semaphore
-  if (semd == -1) { //Error in semaphore
-    printf("No semaphore exists!\n");
-    printf("error %d: %s\n", errno, strerror(errno));
-    semd = semget(KEY, 1, 0);
-    v = semctl(semd, 0, GETVAL, 0); 
-    printf("Semctl returned: %d\n", v); //Semaphore id will be 1
-  }
+  // shmid = shmget(SHMKEY, 0, 0);//Gets shared memory
+  // semd = semget(KEY, 1, 0);//Gets semaphore
+  // if (semd == -1) { //Error in semaphore
+  //   printf("No semaphore exists!\n");
+  //   printf("error %d: %s\n", errno, strerror(errno));
+  //   semd = semget(KEY, 1, 0);
+  //   v = semctl(semd, 0, GETVAL, 0); 
+  //   printf("Semctl returned: %d\n", v); //Semaphore id will be 1
+  // }
 
-  printf("Attempting to open semaphore.\n");
+  // printf("Attempting to open semaphore.\n");
 
-  struct sembuf sb;
-  sb.sem_num = 0;
-  sb.sem_op = -1; //Using the semaphore by downing value of semaphore
-  semop(semd, &sb, 1); //Setting sempahore value to 1 so others can't use it
-  //data = shmat(shmid, 0, 0); //Attaching shmid to data
-  //char lastLine[*data]; //Trying to get the last line of the story by using amount of bytes of shared memory from the last line
-  printf("Semaphore accessed!\n");
-  printf("Enter your data: ");
-  fgets(buffer,sizeof(buffer),stdin); //Copying stdin to buffer
-  int len = strlen(buffer);
-  //write(w_file, buffer, len); //Writing buffer to the file
-  // *data = len;
-  // shmdt(data);
+  // struct sembuf sb;
+  // sb.sem_num = 0;
+  // sb.sem_op = -1; //Using the semaphore by downing value of semaphore
+  // semop(semd, &sb, 1); //Setting sempahore value to 1 so others can't use it
+  // //data = shmat(shmid, 0, 0); //Attaching shmid to data
+  // //char lastLine[*data]; //Trying to get the last line of the story by using amount of bytes of shared memory from the last line
+  // printf("Semaphore accessed!\n");
+  // printf("Enter your data: ");
+  // fgets(buffer,sizeof(buffer),stdin); //Copying stdin to buffer
+  // int len = strlen(buffer);
+  // //write(w_file, buffer, len); //Writing buffer to the file
+  // // *data = len;
+  // // shmdt(data);
 
-  sb.sem_op = 1; //Upping value of semaphore to indicate another program can use it
-  semop(semd, &sb, 1);
+  // sb.sem_op = 1; //Upping value of semaphore to indicate another program can use it
+  // semop(semd, &sb, 1);
 
   // while (1){
     // int server_socket = client_tcp_handshake(IP);
